3bb10cda02ea4681228e28084c839f3c2f673eca
islam shalaby <islam@u-smart.co>
2021-09-20 16:29:51 +0200

    edits 20/9/2021 v7 crone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('serial:update')->everyFiveMinutes();
+        $schedule->command('serial:update')->everyTwoMinutes();
     }
 
     /**
